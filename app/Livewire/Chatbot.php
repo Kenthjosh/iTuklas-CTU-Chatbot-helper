cbf5ff41ab929dbf8a9588c28728e7764d6b72b6
Kenthjosh <oyaokenthjosh@gmail.com>
2025-05-04 02:06:34 +0800

    embedded chatbot, need optimization, proceed with 2fa

--- app/Livewire/Chatbot.php
+++ app/Livewire/Chatbot.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Chat;
 use App\Models\Conversation;
+use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
 class Chatbot extends Component
@@ -20,24 +21,35 @@ class Chatbot extends Component
     {
         if (trim($this->newMessage) === '') return;
 
-        $conversation = Conversation::latest()->first()->id;
+        try {
+            $conversation = Conversation::latest()->first()->id;
 
-        $studentMessage = Chat::create([
-            'conversation_id' => $conversation,
-            'sender' => 'student',
-            'content' => $this->newMessage,
-            'start_date' => now(),
-        ]);
+            $studentMessage = Chat::create([
+                'conversation_id' => $conversation,
+                'sender' => 'student',
+                'content' => $this->newMessage,
+                'start_date' => now(),
+            ]);
 
-        $botReply = Chat::create([
-            'conversation_id' => $conversation,
-            'sender' => 'bot',
-            'content' => 'This is a simulated bot reply.',
-            'start_date' => now(),
-        ]);
+            // $response = Http::post('https://binaryideas.app.n8n.cloud/webhook-test/0a3f0ea6-f2f2-4c1d-badc-287b23d81662', [
+            //     'message' => $this->newMessage
+            // ]);
 
-        $this->messages[] = $studentMessage;
-        $this->messages[] = $botReply;
+            // $botReply = Chat::create([
+            //     'conversation_id' => $conversation,
+            //     'sender' => 'bot',
+            //     'content' => $response->json('reply'),
+            //     'start_date' => now(),
+            // ]);
+
+            // dd($studentMessage);
+            $this->messages[] = $studentMessage;
+            // $this->messages[] = $botReply;
+            $this->messages[] = 'Oops! Something went wrong.';
+        } catch (\Exception $e) {
+            $this->messages[] = $studentMessage;
+            $this->messages[] = 'Oops! Something went wrong.';
+        }
 
         $this->newMessage = '';
     }
